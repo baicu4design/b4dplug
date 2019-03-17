131b54fe2a4172309cb9b2cf76740e590b63386c
baicu4design <baicu4design@gmail.com>
2019-03-17 13:00:05 +0000

    Initial commit
    This is the very first commit for the plugin b4dplug!

---
+++ b4dplug.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: B4DPlug
+Description: Welcome to my b4dplugin
+Plugin URI:  https://www.catalinbaicu.co.uk
+Author: Catalin Baicu
+Version: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/license/gpl-2.0.txt
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 
+2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+*/
+
+
+
+
+
